795c5ef064d7c45776743183c7d60b51d0677abc
Tom Vincent <git@tlvince.com>
2015-06-20 20:53:33 +0100

    feat: add working example

--- app.js
+++ app.js
@@ -2,4 +2,33 @@
 
 angular.module('app', [
   'eha.cordova.google-analytics'
-]);
+])
+  .constant('config', {
+    trackingID: 'UA-000000-01'
+  })
+  .factory('user', function($q, $timeout) {
+    return function() {
+      var deferred = $q.defer();
+      function promise() {
+        return deferred.resolve({
+          id: 'tlvince'
+        });
+      }
+      $timeout(promise, 1000);
+      return deferred.promise;
+    }
+  })
+  .config(function(ehaGoogleAnalyticsProvider, config) {
+    ehaGoogleAnalyticsProvider.trackingID = config.trackingID;
+  })
+  .run(function(user, ehaGoogleAnalytics) {
+    function setUUID(user) {
+      ehaGoogleAnalytics.setUserId(user.id);
+    }
+    user().then(setUUID);
+  })
+  .controller('ctrl', function($scope, ehaGoogleAnalytics) {
+    $scope.click = function() {
+      ehaGoogleAnalytics.trackEvent('category', 'action', 'label');
+    };
+  });
