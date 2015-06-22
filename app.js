5e5dac2ac9d0856c5a56afc5b528816a914601b7
Tom Vincent <git@tlvince.com>
2015-06-22 12:09:05 +0100

    feat: demo exception decorator

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 'use strict';
 
+// Stub `window.analytics` for the purposes of this demo. Usually, you'd be
+// running `eha.google.google-analytics` in a Cordova environment via:
+// https://github.com/danwilson/google-analytics-plugin
+window.analytics = {
+  startTrackerWithId: console.log.bind(console, 'tracker ID:'),
+  trackEvent: console.log.bind(console, 'event:'),
+  setUserId: console.log.bind(console, 'user ID:')
+};
+
 angular.module('app', [
   'eha.cordova.google-analytics'
 ])
   .constant('config', {
+    // Your Google Analytics tracking ID, see:
+    // https://support.google.com/analytics/answer/1032385?hl=en
     trackingID: 'UA-000000-01'
   })
-  .factory('user', function($q, $timeout) {
-    return function() {
+  .service('user', function($q, $timeout) {
+    this.id = function() {
       var deferred = $q.defer();
       function promise() {
         return deferred.resolve({
@@ -16,7 +27,10 @@ angular.module('app', [
       }
       $timeout(promise, 1000);
       return deferred.promise;
-    }
+    };
+    this.login = function() {
+      throw new Error('method unimplemented');
+    };
   })
   .config(function(ehaGoogleAnalyticsProvider, config) {
     ehaGoogleAnalyticsProvider.trackingID = config.trackingID;
@@ -25,10 +39,13 @@ angular.module('app', [
     function setUUID(user) {
       ehaGoogleAnalytics.setUserId(user.id);
     }
-    user().then(setUUID);
+    user.id().then(setUUID);
   })
-  .controller('ctrl', function($scope, ehaGoogleAnalytics) {
-    $scope.click = function() {
+  .controller('ctrl', function($scope, ehaGoogleAnalytics, user) {
+    $scope.trackEvent = function() {
       ehaGoogleAnalytics.trackEvent('category', 'action', 'label');
+    };
+    $scope.trackException = function() {
+      user.login();
     };
   });
